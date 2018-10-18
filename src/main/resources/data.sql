74d163c22c8242222e5bb3ff3e5248c6815cf95a
FlorianBieck <florian@bieck.com>
2018-10-18 12:14:39 +0200

    Data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO multivariate.symbol_relation (idsymbolrelation, symbol, userid) VALUES (1, 'TSLA', 44196397);
+INSERT INTO multivariate.symbol_relation (idsymbolrelation, symbol, userid) VALUES (2, 'FB', 20749410);
+INSERT INTO multivariate.symbol_relation (idsymbolrelation, symbol, userid) VALUES (3, 'TWTR', 12);
